1587a81e6322633677adc1d42cd0d6440c0c604d
SAM <85979367+Mosi-azizi@users.noreply.github.com>
2022-05-14 15:10:50 +0430

    add link

--- resume/views.py
+++ resume/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from .models import Profile,Skill
 # Create your views here.
-def myResume(request):
-    context ={
 
+
+def myResume(request):
+    profiles = Profile.objects.all()
+    skills = Skill.objects.all()
+    context ={
+        'profile':profiles,'skills':skills
     }
     return render(request,'resume/profile.html',context)
