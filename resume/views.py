df8d56faa40a72f0d54eeb32c6bfa24a18ab60c7
SAM <85979367+Mosi-azizi@users.noreply.github.com>
2022-05-22 14:43:50 +0430

    add siteSetting app

--- resume/views.py
+++ resume/views.py
@@ -3,17 +3,20 @@ from django.shortcuts import render, redirect
 from django.utils import http
 
 from .models import Profile,Skill,Education,Employment
+from settings.models import StieSetting
 from .forms import MessageFrom
 # Create your views here.
 
 
 def myResume(request):
     # profiles = Profile.objects.all()
-    profile = Profile.objects.get(username__iexact='sa_azizi')
+    # profile = Profile.objects.get(username__iexact='sa_azizi')
+    profile = Profile.objects.first()
     # skill = profile.skill_set.get(owner__skill='sa_azizi')
     # skill = Skill.objects.all()
     # education = Education.objects.all()
     # experience = Employment.objects.all()
+    siteSetting = StieSetting.objects.first()
     messageForm = MessageFrom()
     if request.method == 'POST':
         messageForm = MessageFrom(request.POST)
@@ -24,7 +27,7 @@ def myResume(request):
 
 
     context ={
-        'profile':profile,'messageForm':messageForm,
+        'profile':profile,'messageForm':messageForm,'siteSetting':siteSetting
         # ,'skill':skill,'education':education,'experience':experience
     }
     return render(request,'resume/profile.html',context)
---
+++ settings/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+
+class StieSetting(models.Model):
+    title = models.CharField(max_length=150, blank=True, null=True)
+    map = models.ImageField(null=True,blank=True)
+    bk_image = models.ImageField(null=True,blank=True)
+    designer = models.CharField(max_length=200, null=True, blank=True)
+    backend_technology = models.CharField(max_length=200, null=True, blank=True)
+    frontend_technology = models.CharField(max_length=200, null=True, blank=True)
+    copy_right = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.title)
+
